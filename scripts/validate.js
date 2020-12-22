bb7b38cfd4fb9cc51cef3c59725fd03292652ef6
Voropay Dmitriy <t79508608460@yandex.ru>
2020-12-23 02:34:29 +0300

    feat: добавить валидацию. оптимизировать разметку

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,20 +1,71 @@
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__tip_active');
+};
 
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_error');
+  errorElement.classList.remove('form__tip_active');
+  errorElement.textContent = '';
+};
 
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
 
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save-button_inactive');
+  }
+  else {
+    buttonElement.classList.remove('popup__save-button_inactive');
+  }
+};
 
+function hasInvalidInput(inputList) {
+  return inputList.some(function(input) {
+    return !input.validity.valid
+  });
+};
 
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector('.form__button-submit');
 
+  toggleButtonState(inputList, buttonElement);
 
+  inputList.forEach(function(inputElement) {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
 
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach(function(formElement) {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
 
-// включение валидации вызовом enableValidation
-// все настройки передаются при вызове
+    setEventListeners(formElement);
+
+  });
+};
 
 enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  formSelector: '.popup__container',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_inactive',
+  inputErrorClass: 'form__input_error',
+  errorClass: 'popup__input-error'
 });
