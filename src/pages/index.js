49089a47ae56d0463ebbbf0b867b538daefed041
Voropay Dmitriy <t79508608460@yandex.ru>
2021-02-09 02:24:57 +0300

    feat: настроить webpack - на пол пути

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import { initialCards } from './initialCards.js';
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
-import { Section } from './Section.js';
-import { PopupWithImage } from './PopupWithImage.js';
-import { PopupWithForm } from './PopupWithForm.js';
-import { UserInfo } from './UserInfo.js';
+import { initialCards } from '../utils/initialCards.js';
+import { Card } from '../components/Card.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { Section } from '../components/Section.js';
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
+import { UserInfo } from '../components/UserInfo.js';
 
 // Передаем классы в переменные
 const editProfileBtn = document.querySelector('.profile__edit-button');
---
+++ src/utils/initialCards.js
@@ -0,0 +1,36 @@
+import taganrogHotel from '../images/element/taganrog-hotel.jpg';
+import taganrogLiceyChehova from '../images/element/taganrog-licey-chehova.jpg';
+import taganrogTum from '../images/element/taganrog-tum.jpg';
+import taganrogCinemaUnost from '../images/element/taganrog-cinema-unost.jpg';
+import taganrogParkGorkogo from '../images/element/taganrog-park-gorkogo.jpg';
+import taganrogBalkaSmerti from '../images/element/taganrog-balka-smerti.jpg';
+
+// Вводим в первоначальный массив с элементами
+const initialCards = [
+  {
+      name: 'Гостиница "Таганрог"',
+      link: taganrogHotel
+  },
+  {
+      name: 'Лицей имени А.П.Чехова',
+      link: taganrogLiceyChehova
+  },
+  {
+      name: 'Таганрогский универмаг',
+      link: taganrogTum
+  },
+  {
+      name: 'Кинотеатр "Юность"',
+      link: taganrogCinemaUnost
+  },
+  {
+      name: 'Парк имени М.Горького',
+      link: taganrogParkGorkogo
+  },
+  {
+      name: 'Балка смерти',
+      link: taganrogBalkaSmerti
+  }
+];
+
+export {initialCards};
